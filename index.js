1e661ed923cd904a4bc8cf72562ef83fe267bfbe
Valeri Karpov <val@karpov.io>
2023-03-16 17:47:29 -0400

    initial

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express');
+const { name, version } = require('./package.json');
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ name, version });
+});
+
+app.listen(port);
+console.log('Listening on port', port);
