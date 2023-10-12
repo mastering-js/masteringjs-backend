d33c32d24d2c8fa651e937f7f978f8a11114a36f
Valeri Karpov <val@karpov.io>
2023-10-12 16:40:22 -0400

    import chunking

--- scripts/importArticles.js
+++ scripts/importArticles.js
@@ -1,13 +1,12 @@
 'use strict';
 
-const Article = require('./src/db/article');
+const Article = require('../src/db/article');
 const articles = require('masteringjs.io/src/tutorials');
 const axios = require('axios');
-const dotenv = require('dotenv');
 const fs = require('fs');
-const mongoose = require('./src/mongoose');
-
-dotenv.config();
+const mongoose = require('../src/mongoose');
+const nlcstToString = require('nlcst-to-string');
+const remark = require('remark');
 
 run().catch(err => console.log(err));
 
@@ -21,15 +20,47 @@ async function run() {
 
   console.log(`Importing ${articles.length} articles`);
   let i = 0;
-  for (const { title, raw, url } of articles) {
-    const content = fs.readFileSync(`${__dirname}/node_modules/masteringjs.io${raw.replace(/^\./, '')}`, 'utf8');
-    const embedding = await createEmbedding(content);
-    await Article.create({
-      title,
-      url,
-      content: `${content.slice(0, 12000)}`,
-      $vector: embedding
+  for (const { title, raw, url, tags } of articles) {
+    if (tags.includes('temporal') || tags.includes('tools')) {
+      continue;
+    }
+    console.log('Importing', title);
+    const content = fs.readFileSync(`${__dirname}/../node_modules/masteringjs.io${raw.replace(/^\./, '')}`, 'utf8');
+    
+    const ast = remark.parse(content);
+    const sections = [{ heading: null, nodes: [] }];
+    let currentSection = 0;
+    ast.children.forEach(node => {
+      if (node.type === 'heading') {
+        ++currentSection;
+        console.log(nlcstToString(node));
+        sections[currentSection] = {
+          heading: nlcstToString(node),
+          nodes: []
+        };
+      } 
+      sections[currentSection].nodes.push(node);
     });
+
+    console.log(`Importing ${sections.length} sections`);
+    for (const section of sections) {
+      const content = remark.stringify({
+        type: 'root',
+        children: section.nodes
+      });
+
+      const embedding = await createEmbedding(content);
+      const contentTitle = section.heading ? `${title}: ${section.heading}` : title;
+      const contentUrl = section.heading ? `${url}#${toKebabCase(section.heading)}` : url;
+
+      await Article.create({
+        title: contentTitle,
+        url: contentUrl,
+        content,
+        $vector: embedding
+      });
+    }
+    
     console.log(`Imported article ${++i} / ${articles.length}`);
   }
 }
@@ -40,11 +71,15 @@ function createEmbedding(input) {
     url: 'https://api.openai.com/v1/embeddings',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`
+      Authorization: `Bearer ${process.env.OPEN_AI_KEY}`
     },
     data: {
       model: 'text-embedding-ada-002',
       input
     }
   }).then(res => res.data.data[0].embedding);
+}
+
+function toKebabCase(str) {
+  return str.toLowerCase().split(/\s+/).map(word => word.replace(/\W+/g, '')).join('-');
 }
